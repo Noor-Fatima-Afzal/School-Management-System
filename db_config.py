6ab95505ae13d9676cdca113746664fb20d7b04f
Noor-Fatima-Afzal <noorfatimaafzalbutt@gmail.com>
2025-05-16 12:08:25 +0500

    updated

--- db_config.py
+++ db_config.py
@@ -5,6 +5,6 @@ def get_connection():
     return mysql.connector.connect(
         host=os.getenv("DB_HOST", "localhost"),    
         user=os.getenv("DB_USER", "root"),           
-        password=os.getenv("DB_PASSWORD", ""),        
+        password=os.getenv("DB_PASSWORD", "noorfatima#01"),        
         database=os.getenv("DB_NAME", "student_db")   
     )
